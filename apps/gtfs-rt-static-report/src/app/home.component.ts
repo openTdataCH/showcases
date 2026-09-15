cbd191b5b5fb31456b51b5b65c920333f9358d1f
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-09-15 23:31:25 +0200

    Updates version

--- apps/gtfs-rt-static-report/src/app/home.component.ts
+++ apps/gtfs-rt-static-report/src/app/home.component.ts
@@ -121,7 +121,7 @@ export class HomeComponent {
       hourCells: [],
       reportValueLookups: reportValueLookups,
       selectedReportValueLookup: reportValueLookups[0],
-      appVersion: '20260509.1',
+      appVersion: '20260915.1',
       showAllHours: false,
       reportLastUpdateF: 'n/a',
       selectedReportMapPrevKeys: {},
