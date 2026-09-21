6073a5ca6f779341e9e7c8a44e14f0d57f047ec1
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-09-21 22:33:49 +0200

    Updates URLs

--- apps/atlas-routes-compare-gtfs/src/app/services/http.service.ts
+++ apps/atlas-routes-compare-gtfs/src/app/services/http.service.ts
@@ -23,7 +23,7 @@ export class HTTP_Service {
   }
 
   async fetchBusinessOrganisationsCSV(): Promise<string> {
-    const url = 'https://opentdatach.github.io/data/business-organisations/full-business-organisation_LATEST.csv';
+    const url = 'https://opentdatach.github.io/data/business-organisation-v2/full-business-organisation_LATEST.csv';
 
     const params = new HttpParams()
       .set('rand', Date.now().toString());
@@ -33,7 +33,7 @@ export class HTTP_Service {
   }
 
   async fetchAtlasLinieCSV(): Promise<string> {
-    const url = 'https://opentdatach.github.io/data/slnid-line-actual-date/actual-date-line_LATEST.csv';
+    const url = 'https://opentdatach.github.io/data/line-v2/actual-date-line_LATEST.csv';
 
     const params = new HttpParams()
       .set('rand', Date.now().toString());
