f1983f3f61efdbec5e4fc23e3f4586e6b23ac8cf
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-09-22 15:35:51 +0200

    Use new BO dataset

--- tools/scripts/cli_otd_fetch_bo.sh
+++ tools/scripts/cli_otd_fetch_bo.sh
@@ -8,8 +8,8 @@ source $DIR/common.sh
 source $PYTHON_VENV_PATH/bin/activate
 
 BO_LOGFILE=$LOGS_BASEPATH/otd_fetch_csv_business-organisations-$DATE_NOW.log
-python3 $DIR/cli_otd_fetch_csv_package.py --package_id business-organisations 2>&1 | tee $BO_LOGFILE
-python3 $DIR/cli_otd_symlink_package.py --package_id business-organisations 2>&1 | tee -a $BO_LOGFILE
+python3 $DIR/cli_otd_fetch_csv_package.py --package_id business-organisation-v2 2>&1 | tee $BO_LOGFILE
+python3 $DIR/cli_otd_symlink_package.py --package_id business-organisation-v2 2>&1 | tee -a $BO_LOGFILE
 symlink_latest $BO_LOGFILE
 
 GO_LOGFILE=$LOGS_BASEPATH/otd_fetch_csv_go-realtime-$DATE_NOW.log
